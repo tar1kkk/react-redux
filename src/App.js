9e4bd0dd09db36dcf3b3c9676f8a0bff50231e4a
Taras <81424893+tar1kkk@users.noreply.github.com>
2022-07-13 18:27:51 +0300

    State, reducer, action + redux hooks useDispatch и useSelector

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useDispatch, useSelector } from 'react-redux'
 
 function App() {
+
+  const dispatch = useDispatch()
+  const cash = useSelector(state => state.cash)
+
+  function addCash(cash) {
+    dispatch({ type: 'ADD_CASH', payload: cash })
+  }
+
+  function getCash(cash) {
+    dispatch({ type: 'GET_CASH', payload: cash })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ fontSize: '3rem' }}>{cash}</div>
+      <div style={{ display: 'flex', margin: '100px 0 0 100px ' }}>
+        <button onClick={() => addCash(Number(prompt()))} style={{ width: '100px', background: 'grey' }}>Пополнить счет</button>
+        <button onClick={() => getCash(Number(prompt()))}>Снять со  счета</button>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,36 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+
+
+
+const defaultState = {
+  cash: 0,
+}
+let action = { type: '', payload: '' }
+
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case 'ADD_CASH':
+      return { ...state, cash: state.cash + action.payload }
+    case "GET_CASH":
+      return { ...state, cash: state.cash - action.payload }
+    default:
+      return state
+  }
+}
+const store = createStore(reducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
