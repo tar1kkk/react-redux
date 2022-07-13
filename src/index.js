9abdd98c39f412ce8fd820654e944db624c3a705
Taras <81424893+tar1kkk@users.noreply.github.com>
2022-07-14 00:04:41 +0300

    Combine Reducers, redux devtools

--- src/index.js
+++ src/index.js
@@ -4,26 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux'
-
-
-
-const defaultState = {
-  cash: 0,
-}
-let action = { type: '', payload: '' }
-
-const reducer = (state = defaultState, action) => {
-  switch (action.type) {
-    case 'ADD_CASH':
-      return { ...state, cash: state.cash + action.payload }
-    case "GET_CASH":
-      return { ...state, cash: state.cash - action.payload }
-    default:
-      return state
-  }
-}
-const store = createStore(reducer)
+import { store } from './store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
