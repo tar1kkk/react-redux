2f238429ff42d502be9c9ead5dcf964dec9714db
Taras <81424893+tar1kkk@users.noreply.github.com>
2022-07-14 13:41:26 +0300

    Action creators. Работа с массивами. Рефакторинг

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { useDispatch, useSelector } from 'react-redux'
+import { addCustomerAction, removeCustomerAction } from './store/customerReducer'
+import { Button } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
+
 
 function App() {
 
   const dispatch = useDispatch()
   const cash = useSelector(state => state.cash.cash)
+  const customers = useSelector(state => state.customers.customers)
 
   function addCash(cash) {
     dispatch({ type: 'ADD_CASH', payload: cash })
@@ -13,14 +18,43 @@ function App() {
   function getCash(cash) {
     dispatch({ type: 'GET_CASH', payload: cash })
   }
+
+  function addCustomer(name) {
+    const customer = {
+      name,
+      id: Date.now(),
+    }
+    dispatch(addCustomerAction(customer))
+  }
+  function removeCustomer(customer) {
+    dispatch(removeCustomerAction(customer.id))
+  }
   return (
     <div className="App">
-      <div style={{ fontSize: '3rem' }}>{cash}</div>
+      <div style={{ fontSize: '3rem', marginLeft: '100px' }}>{cash}</div>
       <div style={{ display: 'flex', margin: '100px 0 0 100px ' }}>
-        <button onClick={() => addCash(Number(prompt()))} style={{ width: '100px', background: 'grey' }}>Пополнить счет</button>
-        <button onClick={() => getCash(Number(prompt()))}>Снять со  счета</button>
+        <Button variant="primary" onClick={() => addCash(Number(prompt()))}>Пополнить счет</Button>
+        <Button variant="secondary" onClick={() => getCash(Number(prompt()))}>Снять со  счета</Button>
+        <Button variant="success" onClick={() => addCustomer(prompt())}>Добавить клиента</Button>
       </div>
-    </div>
+      <div>
+        {customers.length > 0 ?
+          <div>
+            {customers.map(customer => {
+              return (
+                <Card>
+                  <Card.Body ><div onClick={() => removeCustomer(customer)}>{customer.name}</div></Card.Body>
+                </Card>
+              )
+            })}
+          </div>
+          :
+          <div style={{ fontSize: '2rem', marginTop: '20px' }}>
+            Клиенты отсутсвтуют!
+          </div>
+        }
+      </div>
+    </div >
   );
 }
 
